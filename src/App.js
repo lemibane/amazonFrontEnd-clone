cef7d8d6525f5f779362da625b37d409c19e3dac
lemegudata@gmail.com <146845168+Lamibane@users.noreply.github.com>
2024-02-07 15:34:22 +0300

    deploy netlify

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Routing from "./Router.jsx";
 import { auth } from "./Utility/firebase.js";
 import { DataContext } from "./Components/DataProvider/DataProvider.jsx";
 import { Type } from "./Utility/action.type.js";
-
 function App() {
   const [{ user }, dispatch] = useContext(DataContext);
-
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (authUser) {
--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,20 +1,27 @@
-import firebase from "firebase/compat/app";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 // auth
 import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDq8ai7oPPxDSUFM5jSdz6R0C1sfB6Ptzw",
-  authDomain: "frontend-clone-e456e.firebaseapp.com",
-  projectId: "frontend-clone-e456e",
-  storageBucket: "frontend-clone-e456e.appspot.com",
-  messagingSenderId: "288824456943",
-  appId: "1:288824456943:web:fbd15a0a559374b8a6cd1a"
+  apiKey: "AIzaSyCyZ4GXagujiuImHxHRID470qm33lgHPWU",
+  authDomain: "updatedfrontend-clone.firebaseapp.com",
+  projectId: "updatedfrontend-clone",
+  storageBucket: "updatedfrontend-clone.appspot.com",
+  messagingSenderId: "372031083834",
+  appId: "1:372031083834:web:40eb304632a38d7027ac03"
 };
 
 // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
+// all methods needed to work for authentication are available in auth object
 export const auth = getAuth(app);
 export const db = app.firestore();
